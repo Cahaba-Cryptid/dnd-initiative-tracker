4354d5173b4a281c17ff64484258c9b7aa12288a
Hayden <chfrank9011@gmail.com>
2020-05-11 15:15:26 -0500

    Redux revisions.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-import './scss/app';
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import "./scss/app";
+import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import * as serviceWorker from '../serviceWorker';
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
